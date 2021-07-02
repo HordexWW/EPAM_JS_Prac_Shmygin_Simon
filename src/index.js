f2f70a11ac4bab6e93c513696d71a86e780820bb
HordexWW <hordex574@gmail.com>
2021-07-02 04:44:02 +0400

    Task 2: Toolbar

--- src/index.js
+++ src/index.js
@@ -1,3 +1,36 @@
 import "./styles/regular.scss"
 
-console.log("hello world from index.js");
+const allVoicesButton = document.querySelector("#allVoicesButton")
+const microphoneButton = document.querySelector("#microphoneButton")
+const streamButton = document.querySelector("#streamButton")
+const modeText = document.getElementById("mode-status-text")
+
+const activeButtonClassName = "nav__link nav__link--active"
+const inactiveButtonClassName = "nav__link"
+const modeString = "Mode: "
+
+allVoicesButton.onclick = function(){
+    allVoicesButton.className = activeButtonClassName
+    microphoneButton.className = inactiveButtonClassName
+    streamButton.className = inactiveButtonClassName
+
+    modeText.textContent = modeString + "All voices"
+}
+
+microphoneButton.onclick = function(){
+    allVoicesButton.className = inactiveButtonClassName
+    microphoneButton.className = activeButtonClassName
+    streamButton.className = inactiveButtonClassName
+
+    modeText.textContent = modeString + "Microphone"
+}
+
+streamButton.onclick = function(){
+    allVoicesButton.className = inactiveButtonClassName
+    microphoneButton.className = inactiveButtonClassName
+    streamButton.className = activeButtonClassName
+
+    modeText.textContent = modeString + "Stream"
+}
+
+
